e98cf40a537e9a1584d6d66260c76acc1b01c66e
Daniel <lopera.dani@gmail.com>
2018-07-30 16:55:05 -0300

    Se subio workspace.xml

---
+++ abmEmpleados/abmEmpleadoApp/src/main/java/ar/com/tecnosoftware/somos/empleados/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package ar.com.tecnosoftware.somos.empleados.repository;
+
+import ar.com.tecnosoftware.somos.empleados.entity.Cliente;
+
+public interface ClienteRepository {
+
+    public void guardar(Cliente cliente);
+
+}
