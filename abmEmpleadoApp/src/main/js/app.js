ab57eea437754abf42175e24ca2493a7cefeb754
rrojas <rrojas@tecnosoftware.com>
2018-08-02 16:40:04 -0300

    subiendo conexion con react (archivos sin version)

---
+++ abmEmpleadoApp/src/main/js/app.js
@@ -0,0 +1,13 @@
+//app.js
+//console.log("hola, mundo");
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./layout/App.js"
+
+/*
+JSX
+*/
+ReactDOM.render(
+	<App/>,
+	document.getElementById("app")
+	);
