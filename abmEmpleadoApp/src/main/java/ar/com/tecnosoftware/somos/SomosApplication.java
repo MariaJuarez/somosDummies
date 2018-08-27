a47e05c742815072a78c19e693387bca293c87dc
rrojas <rrojas@tecnosoftware.com>
2018-08-27 11:40:32 -0300

    commit de prueba

--- abmEmpleadoApp/src/main/java/ar/com/tecnosoftware/somos/SomosApplication.java
+++ abmEmpleadoApp/src/main/java/ar/com/tecnosoftware/somos/SomosApplication.java
@@ -1,7 +1,7 @@
 package ar.com.tecnosoftware.somos;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SomosApplication {
