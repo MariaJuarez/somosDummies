8307dc43fb21e9cc26192477186964020276e43e
MariaJuarez <juarezmaria2601@gmail.com>
2018-08-29 16:22:40 -0300

    Se deja funcionando la opcion de exportar en EXCEL y descargar el archivo

--- abmEmpleadoApp/src/main/java/ar/com/tecnosoftware/somos/report/ReporteUtil.java
+++ abmEmpleadoApp/src/main/java/ar/com/tecnosoftware/somos/report/ReporteUtil.java
@@ -1,6 +1,9 @@
 package ar.com.tecnosoftware.somos.report;
 
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
@@ -10,10 +13,10 @@ import ar.com.tecnosoftware.somos.report.conexion.Conexion;
 import lombok.Data;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.export.JRXlsExporter;
-import net.sf.jasperreports.engine.export.JRXlsExporterParameter;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.export.SimpleExporterInput;
 import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
-import net.sf.jasperreports.export.SimpleXlsExporterConfiguration;
 import net.sf.jasperreports.export.SimpleXlsReportConfiguration;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
@@ -23,7 +26,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletOutputStream;
+import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletResponse;
 
 @Data
@@ -62,7 +65,7 @@ public class ReporteUtil {
             InputStream inputStream = resource.getInputStream();
             parameters.put("logo", this.getClass().getResourceAsStream(imagen));
             JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, parameters, conexion.conectar());
-           // response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+            response.setContentType(MediaType.APPLICATION_PDF_VALUE);
             JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
             LOG.info("METHOD generarReporte --- Reporte generado Exitosamente");
         } catch (JRException ex) {
@@ -81,7 +84,7 @@ public class ReporteUtil {
             response.setContentType(MediaType.ALL_VALUE);
             JasperExportManager.exportReportToXmlStream(jasperPrint, response.getOutputStream());
 
-                  //s  exportReportToPdfStream(jasperPrint, response.getOutputStream());
+            //s  exportReportToPdfStream(jasperPrint, response.getOutputStream());
             LOG.info("METHOD generarReporte --- Reporte generado Exitosamente");
         } catch (JRException ex) {
             Logger.getLogger(ReporteUtil.class.getName()).log(Level.SEVERE, null, ex);
@@ -90,6 +93,9 @@ public class ReporteUtil {
     }
 
     //exporta a xls
+    /*
+    Leer mas en https://community.jaspersoft.com/wiki/tips-exporting-excel
+     */
     public void exportXls(HttpServletResponse response) throws IOException
     {
         try{
@@ -97,21 +103,49 @@ public class ReporteUtil {
             InputStream inputStream = resource.getInputStream();
             JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, null, conexion.conectar());
             ByteArrayOutputStream output = new ByteArrayOutputStream ();
-            OutputStream outputfile = new FileOutputStream(new File ("classpath:reportes/".concat(reportFileName).concat(".xls")));
+        //    OutputStream outputfile = new FileOutputStream(new File ("classpath:reportes/".concat(reportFileName).concat(".xls")));
 
             // coding For Excel:
             JRXlsExporter exporterXLS = new JRXlsExporter();
             exporterXLS.setExporterInput(new SimpleExporterInput(jasperPrint));
             exporterXLS.setExporterOutput(new SimpleOutputStreamExporterOutput(output));
             SimpleXlsReportConfiguration xlsReportConfiguration = new SimpleXlsReportConfiguration();
-
-            xlsReportConfiguration.setOnePagePerSheet(false);
+            //Sin esto, JASPER solo coloca tantas filas de datos en una sola pestaña como quepan dentro de la Altura de página
+            xlsReportConfiguration.setOnePagePerSheet(true);
+            //Elimina una fila con una altura de fila muy grande al final de la pestaña de datos.
             xlsReportConfiguration.setRemoveEmptySpaceBetweenRows(true);
+            // Elimina las columnas vacías, como las causadas por los márgenes o la distribución deficiente.
+            xlsReportConfiguration.setRemoveEmptySpaceBetweenColumns(true);
+            /*
+            Trata las fechas como fechas, números como números, en lugar de tratar todo como texto
+             */
             xlsReportConfiguration.setDetectCellType(true);
+            /*
+            Muestra las líneas de cuadrícula de Excel, de lo contrario obtendrá un blanco sólido.
+             */
             xlsReportConfiguration.setWhitePageBackground(false);
+            /*
+            Le dice a Excel que no elimine el gráfico del logotipo de la empresa. "true" (es el valor predeterminado) y  elimina los gráficos.
+             */
+            xlsReportConfiguration.setIgnoreGraphics(false);
+           /*
+            Congela el numero de filas superiores que le indiquemos. Una vez en Excel, la fila del nombre del informe,
+             logotipo de la empresa y la fila de los encabezados de las columnas se congelan,
+             y el usuario puede desplazarse por miles de registros y seguir viéndolos.
+             */
+            xlsReportConfiguration.setFreezeRow(4);
 
             exporterXLS.exportReport();
-            outputfile.write(output.toByteArray());
+            LOG.info("METHOD generarReporte --- Reporte generado Exitosamente");
+            //outputfile.write(output.toByteArray());
+
+
+
+
+
+
+
+
          /*   Resource resource = context.getResource("classpath:reportes/".concat(reportFileName).concat(".jasper"));
             InputStream inputStream = resource.getInputStream();
             JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, null, conexion.conectar());
@@ -126,7 +160,7 @@ public class ReporteUtil {
             xlsReportConfiguration.setWhitePageBackground(false);
             xlsExporter.setConfiguration(xlsReportConfiguration);
             xlsExporter.exportReport();*/
-            
+
         }catch(JRException e)
         {
             System.out.println(e);
@@ -134,4 +168,88 @@ public class ReporteUtil {
 
     }
 
-}
+    public void getReporteExcel(HttpServletResponse response) throws IOException {
+
+        Resource resource = context.getResource("classpath:reportes/".concat(reportFileName).concat(".jasper"));
+        InputStream inputStream = resource.getInputStream();
+        //Se definen los parametros si es que el reporte necesita
+
+
+        try {
+           // File file = new File(String.valueOf(resource));
+
+
+            // response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+
+            response.setHeader("Content-Disposition", "attachment; filename=\"reporte.xlsx\";");
+            response.setHeader("Cache-Control", "no-cache");
+            response.setHeader("Pragma", "no-cache");
+            response.setDateHeader("Expires", 0);
+            response.setContentType("application/xlsx");
+
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, parameters, conexion.conectar());
+
+            JRExporter jrExporter = jrExporter = new JRXlsxExporter();
+            jrExporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+                    //setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+            jrExporter.setExporterOutput(new SimpleOutputStreamExporterOutput(response.getOutputStream()));
+                //    setParameter(JRExporterParameter.OUTPUT_STREAM, response.getOutputStream());
+
+            SimpleXlsReportConfiguration xlsReportConfiguration = new SimpleXlsReportConfiguration();
+            xlsReportConfiguration.setOnePagePerSheet(false);
+            xlsReportConfiguration.setRemoveEmptySpaceBetweenRows(true);
+            xlsReportConfiguration.setDetectCellType(true);
+            xlsReportConfiguration.setWhitePageBackground(false);
+            jrExporter.setConfiguration(xlsReportConfiguration);
+
+
+            if (jrExporter != null) {
+                try {
+                    jrExporter.exportReport();
+                } catch (JRException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    /*public void getReporteExcel(HttpServletResponse response) throws ClassNotFoundException, InstantiationException, IllegalAccessException{
+
+        //Se definen los parametros si es que el reporte necesita
+        Map parameter = new HashMap();
+
+        try {
+            Resource resource = context.getResource("classpath:reportes/".concat(reportFileName).concat(".jasper"));
+            InputStream inputStream = resource.getInputStream();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, parameter, conexion.conectar());
+           // File file = new File(ruta);
+
+             response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+
+            response.setHeader("Content-Disposition", "attachment; filename=\"reporte.XLSX\";");
+            response.setHeader("Cache-Control", "no-cache");
+            response.setHeader("Pragma", "no-cache");
+            response.setDateHeader("Expires", 0);
+            response.setContentType("application/XLSX");
+            JRExporter jrExporter = null;
+            jrExporter = new JRXlsxExporter();
+            jrExporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+            jrExporter.setParameter(JRExporterParameter.OUTPUT_STREAM, response.getOutputStream());
+
+            if (jrExporter != null) {
+                try {
+                    jrExporter.exportReport();
+                } catch (JRException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }*/
+
+}}
--- abmEmpleadoApp/src/main/java/ar/com/tecnosoftware/somos/report/controller/ReportController.java
+++ abmEmpleadoApp/src/main/java/ar/com/tecnosoftware/somos/report/controller/ReportController.java
@@ -1,5 +1,6 @@
 package ar.com.tecnosoftware.somos.report.controller;
 
+import ar.com.tecnosoftware.somos.report.EmpleadoBean;
 import ar.com.tecnosoftware.somos.report.ReporteUtil;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
@@ -7,13 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.*;
 
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
 
 @RestController
 @RequestMapping("/report")
 public class ReportController {
 
     private final Log LOG = LogFactory.getLog(ReportController.class);
+
     @Autowired
     ApplicationContext context;
 
@@ -28,12 +34,23 @@ public class ReportController {
                 reporteUtil.generarReportePdf(response);
         }
 
-    @GetMapping(path = "/excel/{reportFileName}")
+    //@GetMapping(path = "/excel/{reportFileName}")
     @ResponseBody
-    public void getExcel(HttpServletResponse response, @PathVariable("reportFileName") String reportFileName) {
+    public void getExcel(HttpServletResponse response, @PathVariable("reportFileName") String reportFileName) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException {
         LOG.info("METHOD: getExcel --- Obteniendo el reporte de ".concat(reportFileName));
         reporteUtil.setReportFileName(reportFileName);
-        reporteUtil.generarReporteExcel(response);
+      //  reporteUtil.getReporteExcel(response);
+    }
+
+
+    @GetMapping(path = "/excel/{reportFileName}")
+    public void verReporteExcel(HttpServletResponse response, @PathVariable("reportFileName") String reportFileName) throws IOException {
+        LOG.info("METHOD: getExcel --- Obteniendo el reporte de ".concat(reportFileName));
+        //Instancia hacia la clase reporteClientes
+        reporteUtil.setReportFileName(reportFileName);
+        reporteUtil.getReporteExcel(response);
+
+
     }
 }
 
