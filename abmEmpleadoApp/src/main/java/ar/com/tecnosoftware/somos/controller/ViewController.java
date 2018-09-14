581070481834d3d5bdd512cbff9d026e4dbfcc9a
mbelizan <mbelizan@tecnosoftware.com>
2018-09-14 12:29:30 -0300

    Subiendo el push con modificaciones elfavicon y en la redirección

--- abmEmpleadoApp/src/main/java/ar/com/tecnosoftware/somos/controller/ViewController.java
+++ abmEmpleadoApp/src/main/java/ar/com/tecnosoftware/somos/controller/ViewController.java
@@ -1,16 +1,15 @@
 package ar.com.tecnosoftware.somos.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+
 public class ViewController {
 
-    @GetMapping("/index")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    @RequestMapping(value="/**/{[path:[^\\\\.]*}")
+    public String index() {
         return "index";
     }
+
 }
